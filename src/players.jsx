1974fa6e6a672c733bcce8e066505f6cd3e557f5
Mohamedamr12 <148327945+Mohamedamr12@users.noreply.github.com>
2024-02-28 12:47:09 +0200

    Add files via upload

---
+++ src/players.jsx
@@ -0,0 +1,36 @@
+
+const players = [
+    {
+      name: "messi",
+      age: 34,
+      position: "leftwing",
+      team: "miami",
+      img: "./messi.jpeg"
+    },
+    {
+      name: "cristiano",
+      age: 36,
+      position: "forward",
+      team: "alnasr",
+      img: "./ronaldo.jpeg"
+    },
+    {
+      name: "sergio ramos",
+      age: 37,
+      position: "Defender",
+      team: "miami",
+      img: "./ramos.jpeg"
+      
+    },
+    {
+      name: "alison",
+      age: 30,
+      position: "Goalkeeper",
+      team: "liverpool",
+      img: "./ali.jpeg"
+      
+    }
+  ];
+  
+  export default players;
+  
